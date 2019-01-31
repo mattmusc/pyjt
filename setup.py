c8c00662a5695713e4110e60beca1c01b32b658c
Matteo Muscella <matteo.muscella@mail.com>
2019-01-31 16:45:15 +0100

    Adds setuptools package description, tests installation.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyjt',
+    version='0.0.1',
+    packages=['pyjt', 'tests'],
+    url='https://github.com/mattmusc/pyjt',
+    license='ISC',
+    author='mattmusc',
+    author_email='matteo.muscella@mail.com',
+    description='Simple program to manipulate JSON'
+)
